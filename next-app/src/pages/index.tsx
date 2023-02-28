ff513869b6a60e54a5e91b5064bfbe5025567167
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2023-02-28 12:55:58 -0500

    feat: after years, bg image

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -1,26 +1,36 @@
 import { GitHubSvg } from "@/components/github-svg";
 import Head from "next/head";
+import Image from "next/image";
+import { Fragment } from "react";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>DocuChat</title>
-        <meta name="description" content="Contextual GitHub Docs Search" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="flex items-center justify-center w-screen h-screen">
-        <div className="flex flex-col items-center justify-center gap-9">
-          <GitHubSvg className="w-24" />
-          <div>
-            <input className="hidden" type="text" />
-            <button className="font-semibold text-lg bg-gray-200 px-6 py-4 rounded-full">
-              ask me
-            </button>
-          </div>
-        </div>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>DocuChat</title>
+                <meta name="description" content="Contextual GitHub Docs Search" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Fragment>
+                <Image
+                    fill
+                    className="object-cover"
+                    alt=""
+                    src={"/background.png"}
+                ></Image>
+                <main className="flex items-center justify-center w-screen h-screen absolute z-10">
+                    <div className="flex flex-col items-center justify-center gap-9">
+                        <GitHubSvg className="w-24" />
+                        <div>
+                            <input className="hidden" type="text" />
+                            <button className="font-semibold text-lg bg-gray-200 px-6 py-4 rounded-full">
+                                ask me
+                            </button>
+                        </div>
+                    </div>
+                </main>
+            </Fragment>
+        </>
+    );
 }
